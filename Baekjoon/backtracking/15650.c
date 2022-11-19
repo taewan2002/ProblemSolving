7873e0c9ca5fdebb2137a4f0e6605d89fd0e84d4
taewan <taewan2002@gachon.ac.kr>
2022-11-19 16:20:48 +0900

    dfs, backtracking

--- Baekjoon/backtracking/15650.c
+++ Baekjoon/backtracking/15650.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int check_upperbound(int *result, int m){
+    for(int i=0; i<m-1; i++){
+        if(result[i] > result[i+1]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void dfs(int depth, int n, int m, int *check, int *result){
     if(depth == m){
-        for(int i=0; i<m; i++){
-            printf("%d ", result[i]);
+        if(check_upperbound(result, m)){
+            for(int i=0; i<m; i++){
+                printf("%d ", result[i]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
     else{
         for (int i = 0; i < n; i++){
