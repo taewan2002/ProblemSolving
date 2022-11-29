b0fec4dcc597473258130955308f0f6182724d45
taewan <taewan2002@gachon.ac.kr>
2022-11-30 05:17:27 +0900

    greegy algorithm

---
+++ Baekjoon/greedy/1026.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+
+list1.sort()
+
+total = 0
+for i in range(n):
+    total += min(list1) * max(list2)
+    list1.pop(list1.index(min(list1)))
+    list2.pop(list2.index(max(list2)))
+    
+print(total)
