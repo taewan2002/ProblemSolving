b1604c82a2c09e5b9f022a5dafbbd1e2b7f38b47
taewan <taewan2002@naver.com>
2022-12-22 14:57:10 +0900

    two pointer

---
+++ Baekjoon/math/3273.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); // c++ 만의 독립버퍼로 시간이 빨라진다.
+    cin.tie(nullptr); // cin cout가 서로 묶여있다... 그것을 풀어줌(?)
+
+	int N, tmp, total;
+	cin >> N;
+	vector<int> arr;
+	while(N--){
+		cin >> tmp;
+		arr.push_back(tmp);
+	}
+	sort(arr.begin(), arr.end()); // 1 2 3 5 7 9 10 11 12
+	cin >> total;
+
+	int cnt=0, sum=0, lo=0, hi = arr.size()-1;
+	while(lo<hi){
+		sum = arr[lo] + arr[hi];
+		if(sum > total) hi--;
+		else if(sum < total) lo++;
+		else{
+			cnt++;
+			hi--;
+			lo++;
+		}
+	}
+	cout << cnt << endl;
+}
