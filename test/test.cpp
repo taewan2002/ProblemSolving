c24c3e65fd3eee69f3590a3c66c97a2d16e8ed64
taewan <taewan2002@naver.com>
2022-12-22 14:16:59 +0900

    two pointer

--- test/test.cpp
+++ test/test.cpp
@@ -3,34 +3,31 @@
 #include<vector>
 using namespace std;
 
-int N, cnt=0;
-
 int main() {
 	ios_base::sync_with_stdio(false); // c++ 만의 독립버퍼로 시간이 빨라진다.
     cin.tie(nullptr); // cin cout가 서로 묶여있다... 그것을 풀어줌(?)
 
-	cin >> N;
-	// 이렇게 풀면 시간초과 남..
-	// for(int i=1; i<(N/2 +2); i++){
-	// 	int total=0;
-	// 	for(int k=i; k<(N/2 +2); k++){
-	// 		total += k;
-	// 		if(total == N) cnt++;
-	// 	}
-	// }
-	int left=1, right=1, total=0;
-	while(left <= right && right <= N+1){
-		if(total > N){
-			total -= left;
+	int N, M, cnt=0, a;
+	vector<int> arr;
+
+	cin >> N >> M;
+	for(int i=0; i<N; i++){
+		cin >> a;
+		arr.push_back(a);
+	}
+	int left=0, right=0, total=0;
+	while(left <= right && right <= N){
+		if(total > M){
+			total -= arr[left];
 			left++;
 		}
-		else if(total < N){
-			total += right;
+		else if(total < M){
+			total += arr[right];
 			right++;
 		}
-		else if(total == N){
+		else if(total == M){
 			cnt++;
-			total += right;
+			total += arr[right];
 			right++;
 		}
 	}
