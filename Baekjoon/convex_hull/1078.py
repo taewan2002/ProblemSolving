7335483f6bb0ed8c114490ffd46eb71327d0ab0c
taewan <taewan2002@naver.com>
2022-12-25 19:08:08 +0900

    convex_hull

---
+++ Baekjoon/convex_hull/1078.py
@@ -0,0 +1,21 @@
+def ccw(p1, p2, p3):
+    return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
+
+def convex_hull(points):
+    points = sorted(points)
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and ccw(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+    return lower[:-1] + upper[:-1]
+
+n = int(input())
+points = [list(map(int, input().split())) for _ in range(n)]
+
+print(len(convex_hull(points)))
