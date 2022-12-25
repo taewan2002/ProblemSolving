96c40d538934da045db1fd88b05cd8086b1d908f
taewan <taewan2002@naver.com>
2022-12-25 19:08:04 +0900

    convex_hull

--- test/test.py
+++ test/test.py
@@ -1,39 +1,21 @@
-# import sys
-# input = sys.stdin.readline
- 
+def ccw(p1, p2, p3):
+    return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
+
+def convex_hull(points):
+    points = sorted(points)
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and ccw(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+    return lower[:-1] + upper[:-1]
+
 n = int(input())
-str1 = str(input().strip())
- 
-dic = {}
-result = [0, 0]
-start = 0
-end = 0
- 
-while start < len(str1) and end < len(str1):
- 
-    if str1[end] not in dic:
-        dic[str1[end]] = 1
-    else:
-        dic[str1[end]] += 1
- 
-    # 만약 딕셔너리안에 n개보다 더 많은 알파벳이 들어갔을경우
-    # start값을 올려서 n개가 되도록 알파벳을 빼줘야한다.
-    if len(dic) > n:
-        while start <= end and len(dic) > n:
-            if dic[str1[start]] == 1:
-                dic.pop(str1[start])
-            else:
-                dic[str1[start]] -= 1
-            start += 1
- 
-    # 만약 연속된 n개의 문자열이 들어왔을경우
-    # result안에 최대 길이가 들어갈 수 있도록 넣어준다.
-    if len(dic) <= n:
-        # 최대길이로 갱신해주자~
-        if result[1] - result[0] < end - start:
-            result[0] = start
-            result[1] = end
- 
-    end += 1
- 
-print(result[1] - result[0] + 1)
+points = [list(map(int, input().split())) for _ in range(n)]
+
+print(len(convex_hull(points)))
