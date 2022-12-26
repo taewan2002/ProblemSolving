6ed4b662e49d6f4afe229ffdcccd060a72ccca73
taewan <taewan2002@naver.com>
2022-12-26 15:00:50 +0900

    bubble_sort

--- test/test.cpp
+++ test/test.cpp
@@ -3,46 +3,39 @@
 #include<vector>
 using namespace std;
 
+void insertion_sort(vector<int> &arr, int K){
+	int N = arr.size();
+	int cnt = 0;
+	for(int i=1; i<N; i++){
+		int loc = i-1;
+		int newItem = arr[i];
+		while(0 <= loc && newItem < arr[loc]){
+			arr[loc+1] = arr[loc];
+			cnt++;
+			if(cnt == K){
+				cout << arr[loc] << endl;
+				return;
+			}
+			loc--;
+		}
+		if(loc+1 != i){
+			arr[loc+1] = newItem;
+			cnt++;
+		}
+	}
+	cout << -1 << endl;
+	
+}
+
 int main() {
 	ios_base::sync_with_stdio(false); // c++ 만의 독립버퍼로 시간이 빨라진다.
     cin.tie(nullptr); // cin cout가 서로 묶여있다... 그것을 풀어줌(?)111111
 
-	int N, tmp;
-	vector<int> arr;
-	cin >> N;
+	int N, K;
+	cin >> N >> K;
+	vector<int> arr(N);
 	for(int i=0; i<N; i++){
-		cin >> tmp;
-		arr.push_back(tmp);
+		cin >> arr[i];
 	}
-	sort(arr.begin(), arr.end());
-
-	// 자꾸 시간 초과나는데 어떡해...?
-	int lo=0, hi=N-1, total, cnt=0;
-	for(int i=0; i<N; i++){
-		total = arr[i]; // 두 수의 합이 돼야 할 수
-		while(lo < hi){
-			// total을 두수의 합으로 만들 수 있으면 cnt를 올린다.
-			if (lo == i){
-				lo++;
-				continue;
-			}
-			else if(hi == i){
-				hi--;
-				continue;
-			}
-			if(arr[lo] + arr[hi] == total){
-				cnt++;
-				break;
-			}
-			else if(arr[lo] + arr[hi] > total){
-				hi--;
-			}
-			else{
-				lo++;
-			}
-		}
-		lo=0;
-		hi = N-1;
-	}
-	cout << cnt << endl;
+	insertion_sort(arr, K);
 }
