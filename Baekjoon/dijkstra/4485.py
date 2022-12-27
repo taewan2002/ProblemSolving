fecb17f7e54f6e5a77827e57292ddc57815bbf88
taewan <taewan2002@naver.com>
2022-12-27 16:52:14 +0900

    dijkstra

--- Baekjoon/dijkstra/4485.py
+++ Baekjoon/dijkstra/4485.py
@@ -1,3 +1,37 @@
+import heapq
+import sys
 
-        
-        
+input = sys.stdin.readline
+INF = int(1e9)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1] 
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, (graph[0][0], 0, 0)) # cost, x, y
+    distance[0][0] = 0 # 시작점
+
+    while q: # q가 비어있지 않다면
+        cost, x, y = heapq.heappop(q) # cost, x, y
+        if x == n - 1 and y == n - 1: # 도착점
+            print(f'Problem {count}: {distance[x][y]}')
+            break
+        for i in range(4):
+            new_x = x + dx[i] # 다음 x좌표
+            new_y = y + dy[i] # 다음 y좌표
+            if 0 <= new_x < n and 0 <= new_y < n: # 범위 안에 있다면
+                new_cost = cost + graph[new_x][new_y] # 새로운 cost
+                if new_cost < distance[new_x][new_y]: # 새로운 cost가 기존 cost보다 작다면
+                    distance[new_x][new_y] = new_cost # 기존 cost를 새로운 cost로 갱신
+                    heapq.heappush(q, (new_cost, new_x, new_y)) # q에 새로운 cost, x, y를 넣음
+
+count = 1
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    distance = [[INF] * n for _ in range(n)]
+    dijkstra()
+    count += 1
--- test/test.py
+++ test/test.py
@@ -1,30 +1,52 @@
+# https://www.acmicpc.net/problem/10217
+# KCM Travel
+# 시간제한 : 2초, 메모리 제한 : 256MB
+
+# 반례
+# 입력
+# 1
+# 6 149 8
+# 1 2 60 20
+# 2 3 30 70
+# 1 3 100 80
+# 1 3 20 180
+# 3 4 20 100
+# 3 5 150 20
+# 5 6 50 40
+# 4 6 30 50
+# 출력
+# 240
+
+# 코드
 import sys
+import heapq
 input = sys.stdin.readline
 
-def ccw(p1, p2, p3):
-    return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost <= m and cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-def convex_hull(points):
-    points = sorted(points)
-    lower = []
-    for p in points:
-        while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) < 0:
-            lower.pop()
-        lower.append(p)
-    upper = []
-    for p in reversed(points):
-        while len(upper) >= 2 and ccw(upper[-2], upper[-1], p) < 0:
-            upper.pop()
-        upper.append(p)
-    return lower[:-1] + upper[:-1]
+t = int(input())
+for _ in range(t):
+    n, m, k = map(int, input().split())
+    graph = [[] for _ in range(n+1)]
+    distance = [sys.maxsize] * (n+1)
+    for _ in range(k):
+        u, v, c, d = map(int, input().split())
+        graph[u].append((v, c, d))
+    dijkstra(1)
+    if distance[n] == sys.maxsize:
+        print('Poor KCM')
+    else:
+        print(distance[n])
 
-n = int(input())
-points = []
-for _ in range(n):
-    x,y,c = input().split()
-    if c == 'Y':
-        points.append([int(x), int(y)])
-
-print(len(convex_hull(points)))
-for p in convex_hull(points):
-    print(p[0], p[1])
