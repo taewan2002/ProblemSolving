8c6e9be3c8f2ea5ae95a04a54e8208d15df0a0dd
taewan <taewan2002@naver.com>
2022-12-27 18:06:23 +0900

    prime num

--- test/test.py
+++ test/test.py
@@ -1,52 +1,24 @@
-# https://www.acmicpc.net/problem/10217
-# KCM Travel
-# 시간제한 : 2초, 메모리 제한 : 256MB
-
-# 반례
-# 입력
-# 1
-# 6 149 8
-# 1 2 60 20
-# 2 3 30 70
-# 1 3 100 80
-# 1 3 20 180
-# 3 4 20 100
-# 3 5 150 20
-# 5 6 50 40
-# 4 6 30 50
-# 출력
-# 240
-
-# 코드
 import sys
-import heapq
 input = sys.stdin.readline
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost <= m and cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+def solution(MIN,MAX):
+    answer = MAX-MIN+1
+    check = [False]*(MAX-MIN+1) # MIN~MAX까지의 수를 체크할 배열
+    i=2
+    while i*i <= MAX:
+        square_number = i*i
+        # MIN이 square_number의 배수가 아니라면, MIN이 속한 square_number의 첫번째 수를 구해준다.
+        remain = 0 if MIN%square_number==0 else 1 
+        j = MIN//square_number + remain
+        while square_number*j <= MAX:
+            # 이미 체크된 수는 넘어간다.
+            if not check[square_number*j-MIN]:
+                check[square_number*j-MIN]=True
+                answer-=1
+            j+=1
+        i+=1        
+    print(answer)
+    
 
-t = int(input())
-for _ in range(t):
-    n, m, k = map(int, input().split())
-    graph = [[] for _ in range(n+1)]
-    distance = [sys.maxsize] * (n+1)
-    for _ in range(k):
-        u, v, c, d = map(int, input().split())
-        graph[u].append((v, c, d))
-    dijkstra(1)
-    if distance[n] == sys.maxsize:
-        print('Poor KCM')
-    else:
-        print(distance[n])
-
+a,b = map(int,input().split())
+solution(a,b)
