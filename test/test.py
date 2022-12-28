3e62afceec21dfaa26099b5724c2ff69eeba7ad6
taewan <taewan2002@naver.com>
2022-12-29 03:48:17 +0900

    [pass] 트리의 순회

--- test/test.py
+++ test/test.py
@@ -1,41 +1,36 @@
 import sys
-input = sys.stdin.readline
-
-def lower_bound(key):
-    global total_list
-    start = 0
-    end = len(total_list) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if total_list[mid] < key:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return start
-
-
-def get_max(node):
-    global total_list
-    global high
-    lb = lower_bound(node)
-    if lb > 0:
-        left = high[total_list[lb - 1]]
-    else:
-        left = 0
-    if lb < len(total_list):
-        right = high[total_list[lb]]
-    else:
-        right = 0
-    high[node] = max(left, right) + 1
-    total_list.insert(lb, node)
-
-    return high[node]
-
-
-n = int(input())
-total = 0
-total_list = []
-high = [0] * (n + 1)
-for i in range(n):
-    total += get_max(int(input()))
-print(total)
+ 
+N = int(sys.stdin.readline().strip())
+tree = {}
+ 
+for n in range(N):
+    root, left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left, right]
+ 
+ 
+def preorder(root):
+    if root != '.':
+        print(root, end='')  # root
+        preorder(tree[root][0])  # left
+        preorder(tree[root][1])  # right
+ 
+ 
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])  # left
+        print(root, end='')  # root
+        inorder(tree[root][1])  # right
+ 
+ 
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])  # left
+        postorder(tree[root][1])  # right
+        print(root, end='')  # root
+ 
+ 
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
