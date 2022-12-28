e15c8e84bee96da1e284c26acab9dd3102b5875e
taewan <taewan2002@naver.com>
2022-12-28 17:49:13 +0900

    에라토스테네스의 체

--- test/test.cpp
+++ test/test.cpp
@@ -4,39 +4,30 @@
 #include<math.h>
 using namespace std;
 
-int t=0;
-int min_num;
-bool isPrime(int num) {
-    if(num < 2) return false;
-
-	// 2부터 num의 제곱근까지 나눠보면 된다.
-	// 시간복잡도는 O(sqrt(n))
-    for(int i = 2 ; i <= sqrt(num) ; i++) {
-        if(num % i == 0) return false;
-    }
-	if (t == 0) {
-		min_num = num;
-		t++;
-	}
-    return true;
-}
-
 int main() {
 	ios_base::sync_with_stdio(false); // c++ 만의 독립버퍼로 시간이 빨라진다.
     cin.tie(nullptr); // cin cout가 서로 묶여있다... 그것을 풀어줌(?)
 
-	int a, b;
-	cin >> a >> b;
-	int cnt = 0;
+	while(1){
+		int N;
+		cin >> N;
+		if(N == 0) break;
+		vector<int> arr(2*N+1, 0);
+		int cnt = 0;
+		// 에라토스테네스의 체
+		for(int i=2; i<=sqrt(2*N+1); i++){
+			for(int k=i; k<=2*N+1; k+=i){
+				if(arr[k] == 0){
+					arr[k] = 1;
+				}
+			}
+		}
 
-    for(int i = a ; i <= 10000000 ; i++) {
-        if(i > b) break;
-        if(isPrime(i)) {
-            cnt += i;
-        }
-    }
-	if(cnt != 0) cout << cnt << "\n" << min_num << endl;
-	else cout << -1 << endl;
+		for(int i=N+1; i<=N*2; i++){
+			if(arr[i] == 0) cnt++;
+		}
 
+		cout << cnt << endl;
+	}
     return 0;
 }
