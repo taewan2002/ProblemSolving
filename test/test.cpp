e4b0772d3c0e0b5fe8c66a95107288e6b70b33e3
taewan <taewan2002@naver.com>
2022-12-29 17:10:22 +0900

    [pass] 이진검색트리, postorder

--- test/test.cpp
+++ test/test.cpp
@@ -4,42 +4,23 @@
 #include<math.h>
 using namespace std;
 
-int N, K;
-int gold[1001];
-int silver[1001];
-int bronze[1001];
-int res = 0;
-
 int main() {
 	ios_base::sync_with_stdio(false); // c++ 만의 독립버퍼로 시간이 빨라진다.
     cin.tie(nullptr); // cin cout가 서로 묶여있다... 그것을 풀어줌(?)
 
-	//입력
-    cin >> N >> K;
-    for (int i = 0; i < N; i++) {
-        int index;
-        cin >> index;
-        cin >> gold[index] >> silver[index] >> bronze[index];
-    }
- 
-    //1번 국가부터 N번 국가까지 K번 국가보다 더 잘한 국가일 경우 res++
-    for (int i = 1; i <= N; i++) {
-        if (gold[i] > gold[K]) {
-            res++;
-        }
-        else if (gold[i] == gold[K]) {
-            if (silver[i] > silver[K]) {
-                res++;
-            }
-            else if (silver[i] == silver[K]) {
-                if (bronze[i] > bronze[K]) {
-                    res++;
-                }
-            }
-        }
-    }
- 
-    //출력
-    cout << res + 1;
+	int N;
+
+    cin >> N;
+	vector<int> arr;
+	for(int i=0; i<N; i++){
+		int temp;
+		cin >> temp;
+		arr.push_back(temp);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	cout << arr[(N-1)/2] << endl;
+    
     return 0;
 }
