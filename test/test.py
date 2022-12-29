47f093be0c4e64c3d4c2383202bdc5b9cd35a374
taewan <taewan2002@naver.com>
2022-12-29 23:45:53 +0900

    [pass] hashMap

--- test/test.py
+++ test/test.py
@@ -1,36 +1,19 @@
-import sys
- 
-N = int(sys.stdin.readline().strip())
-tree = {}
- 
-for n in range(N):
-    root, left, right = sys.stdin.readline().strip().split()
-    tree[root] = [left, right]
- 
- 
-def preorder(root):
-    if root != '.':
-        print(root, end='')  # root
-        preorder(tree[root][0])  # left
-        preorder(tree[root][1])  # right
- 
- 
-def inorder(root):
-    if root != '.':
-        inorder(tree[root][0])  # left
-        print(root, end='')  # root
-        inorder(tree[root][1])  # right
- 
- 
-def postorder(root):
-    if root != '.':
-        postorder(tree[root][0])  # left
-        postorder(tree[root][1])  # right
-        print(root, end='')  # root
- 
- 
-preorder('A')
-print()
-inorder('A')
-print()
-postorder('A')
+n=int(input())
+for i in range(n):
+    com=int(input())
+    wears={}
+    for i in range(com):
+        a,b=input().split()
+        if b in wears:
+            wears[b].append(a)
+        else:
+            wears[b]=[a]
+    a=[]
+    for key,values in wears.items():
+        a.append(len(values)+1)
+    
+    ans=1
+    for i in a:
+        ans*=i
+    
+    print(ans-1)
