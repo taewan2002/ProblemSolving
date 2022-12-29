bb284472e126683ccbc9ba096663731653af64f4
taewan <taewan2002@naver.com>
2022-12-29 17:10:16 +0900

    [pass] 이진검색트리, postorder

--- Baekjoon/tree/5639.py
+++ Baekjoon/tree/5639.py
@@ -1,36 +1,34 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-class binary_tree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+# 후위 순회, postorder
+# 왼쪽 서브트리 -> 오른쪽 서브트리 -> 루트 노드
+# 왼쪽 서브트리의 루트 노드 < 루트 노드 < 오른쪽 서브트리의 루트 노드
 
-    def insert_left(self, value):
-        self.left = binary_tree(value)
-        return self.left
+def postorder(start, end):
+    # end = 마지막 인덱스
+    if start > end:
+        return
+    root = num_list[start] # 현재 노드
+    for i in range(start + 1, end + 1): 
+        if root < num_list[i]: # 오른쪽 서브트리
+            postorder(start + 1, i - 1) # 왼쪽 서브트리
+            postorder(i, end) # 오른쪽 서브트리
+            print(root)
+            return
+    # 오른쪽 서브트리가 없는 경우
+    postorder(start + 1, end) # 왼쪽 서브트리
+    print(root) # 루트 노드 출력
 
-    def insert_right(self, value):
-        self.right = binary_tree(value)
-        return self.right
+num_list = []
+while True:
+    # 입력이 주어지지 않으면 postorder 함수를 호출한다.
+    try:
+        num = int(input())
+        num_list.append(num)
+    except:
+        postorder(0, len(num_list) - 1)
+        break
 
-def insert_node(node, value):
-    global total
-    if value < node.value:
-        if node.left == None:
-            node.insert_left(value)
-        else:
-            insert_node(node.left, value)
-    else:
-        if node.right == None:
-            node.insert_right(value)
-        else:
-            insert_node(node.right, value)
 
-N = int(input())
-tree = binary_tree(int(input()))
-
-for i in range(N-1):
-    value = int(input())
-    insert_node(node, value)
