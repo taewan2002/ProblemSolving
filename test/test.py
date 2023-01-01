30eb72c940017e003e4fe8356a8397ecefc5055f
taewan <taewan2002@naver.com>
2023-01-02 04:14:42 +0900

    [pass] brute_force

--- test/test.py
+++ test/test.py
@@ -1,20 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-N, M = list(map(int, input().split()))
-dp = [list(map(int, input().split())) for _ in range(N)]
+N = int(input())
+M = int(input())
+if M:
+    broken = list(map(int, input().split()))
+else:
+    broken = []
 
-for i in range(N):
-    if i == 0:
-        continue
-    for k in range(M):
-        if dp[i][k] == 0:
-            continue
-        if k == 0:
-            dp[i][0] = dp[i-1][0] + dp[i-1][1]
-        elif k == M-1:
-            dp[i][-1] = dp[i-1][-1] + dp[i-1][-2]
-        else:
-            dp[i][k] = dp[i-1][k-1] + dp[i-1][k] + dp[i-1][k+1]
+target = abs(100 - N)
+for i in range(1000001):
+    num = str(i)
+    for k in range(len(num)):
+        if int(num[k]) in broken:
+            break
+        elif k == len(num)-1:
+            target = min(target, abs(i - N) + len(num))
 
-print(sum(dp[N-1])%1000000007)
+print(target)
