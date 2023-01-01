67898ba08f2f7f2273dc730908ab45971eb1fe0e
taewan <taewan2002@naver.com>
2023-01-01 13:10:45 +0900

    [pass] bfs

--- test/test.cpp
+++ test/test.cpp
@@ -5,55 +5,57 @@
 #include<string>
 #include<set>
 #include<map>
+#include<queue>
 using namespace std;
 
 #define INF 987654321
+int F, S, G, U, D;
+int path[1000001];
+bool visited[1000001];
+vector<int> arr;
+queue<int> q;
+
+void BFS(int v){
+	visited[v] = true;
+	q.push(v);
+
+	while(!q.empty()){
+		v = q.front();
+		q.pop();
+
+		for(int i=0; i<2; i++){
+			int nv = v + arr[i];
+
+			if(0 < nv && nv <= F){
+				if(!visited[nv]){
+					visited[nv] = true;
+					q.push(nv);
+					path[nv] = path[v] + 1;
+				}
+			}
+		}
+	}
+}
+
+
 
 int main() {
 	ios_base::sync_with_stdio(false); // c++ 만의 독립버퍼로 시간이 빨라진다.
     cin.tie(nullptr); // cin cout가 서로 묶여있다... 그것을 풀어줌(?)
+	
+	
+	cin >> F >> S >> G >> U >> D;
 
-	int N, M;
-	cin >> N >> M;
+	arr.push_back(U);
+	arr.push_back(D * -1);
 
-	int arr[101][101];
-	// arr테이블 초기화
-	for(int i=1; i<=N; i++){
-		for(int j=1; j<=N; j++){
-			arr[i][j] = INF;
-		}
+	BFS(S);
+
+	if(visited[G]){
+		cout << path[G] << endl;
 	}
-
-	// 그래프 입력
-	int from, to, weight;
-	for(int i=0; i<M; i++){
-		cin >> from >> to >> weight;
-		if(arr[from][to] > weight){
-			arr[from][to] = weight;
-		}
-	}
-
-	// floyd-warshall
-	for(int i=1; i<=N; i++){ // i vertex를 거치는 경우
-		for(int j=1; j<=N; j++){ // 시작 vertex
-			for(int k=1; k<=N; k++){ // 도착 vertex
-				if(arr[j][i] != INF && arr[i][k] != INF)
-					arr[j][k] = min(arr[j][k], arr[j][i] + arr[i][k]);
-			}
-		}
-	}
-
-	// 출력
-	for(int i=1; i<=N; i++){
-		for(int j=1; j<=N; j++){
-			if(i==j || arr[i][j] == INF){
-				cout << 0 << " ";
-			}
-			else{
-				cout << arr[i][j] << " ";
-			}
-		}
-		cout << endl;
+	else{
+		cout << "use the stairs" << endl;
 	}
 
     return 0;
