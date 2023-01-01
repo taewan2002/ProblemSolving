e87798d02013a54e6f9bb8401fd5a28b650b8cd7
taewan <taewan2002@naver.com>
2023-01-02 02:09:04 +0900

    [pass] greedy

--- test/test.py
+++ test/test.py
@@ -1,6 +1,10 @@
-import math
 import sys
 input = sys.stdin.readline
 
-A, B = map(int, input().split())
-print("1" * math.gcd(A, B))
+N = int(input())
+num_list = sorted(list(map(int, input().split())))
+min_value = 1e9
+for i in range(N):
+    if min_value > num_list[i] + num_list[-1-i]:
+        min_value = num_list[i] + num_list[-1-i]
+print(min_value)
