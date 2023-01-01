cffcf80ca7f7a57fa93ead0777532936520afcaa
taewan <taewan2002@naver.com>
2023-01-01 22:41:18 +0900

    [pass] priority_queue

--- test/test.cpp
+++ test/test.cpp
@@ -8,63 +8,39 @@
 #include<queue>
 using namespace std;
 
-bool compare(const pair<int, string> &a, const pair<int, string> &b){
-	if(a.first == b.first){
-		if(a.second.length() < b.second.length()){
-			return a.second < b.second;
-		}
-		else if(a.second.length() > b.second.length()){
+struct cmp {
+	bool operator()(pair<int, string> a, pair<int, string> b) {
+		if (a.first == b.first)
 			return a.second > b.second;
-		}
-		else{
-			if(a.second < b.second){
-				return a.second < b.second;
-			}
-			else{
-				return a.second > b.second;
-			}
-		}
+		return a.first <= b.first;
 	}
-	return a.first < b.first;
-}
-
+};
 
 int main() {
 	ios_base::sync_with_stdio(false); // c++ 만의 독립버퍼로 시간이 빨라진다.
     cin.tie(nullptr); // cin cout가 서로 묶여있다... 그것을 풀어줌(?)
 	
-	vector<pair<int, string>> blue;
-	vector<pair<int, string>> white;
+	int N;
+	priority_queue<pair<int, string>, vector<pair<int, string>>, cmp> q;
+	string str, tmp;
+	cin >> str;
+	cin >> N;
+	while(N--){
+		cin >> tmp;
+		int L=0, O=0, V=0, E=0;
+		for(auto c:str+tmp){
+			switch(c){
+				case 'L': L++; break;
+				case 'O': O++; break;
+				case 'V': V++; break;
+				case 'E': E++; break;
 
-	int N, M, a;
-	string b;
-	cin >> N >> M;
-	int *cnt = new int[N+1];
-	for(int i=0; i<N+1; i++){
-		cnt[i] = 0;
-	}
-	while(true){
-		cin >> a >> b;
-		if(a==0 && b[0]=='0') break;
-		if(a%2==0 && cnt[a] < M){
-			white.push_back(make_pair(a, b));
-			cnt[a]++;
+			}
 		}
-		else if(a%2!=0 && cnt[a]<M){
-			blue.push_back(make_pair(a,b));
-			cnt[a]++;
-		}
-	}
-	sort(white.begin(), white.end(), compare);
-	sort(blue.begin(), blue.end(), compare);
-
-	for(auto it: blue){
-		cout << it.first << " " << it.second << endl;
-	}
-	for(auto it: white){
-		cout << it.first << " " << it.second << endl;
+		int total = ((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100;
+		q.push({total, tmp});
 	}
 
-	delete[] cnt;
+	cout << q.top().second << endl;
     return 0;
 }
