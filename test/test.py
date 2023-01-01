eaa5fc850af2edd46a36288237dc2f38599ab3cc
taewan <taewan2002@naver.com>
2023-01-02 01:21:44 +0900

    [pass] hashmap

--- test/test.py
+++ test/test.py
@@ -2,13 +2,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-total = {}
+total = []
 for _ in range(n):
-    tmp = int(input())
-    if tmp in total:
-        total[tmp] += 1
-    else:
-        total[tmp] = 1
+    a, b = input().split()
+    total.append([a, int(b)])
 
 # value는 높은 순서대로, key는 낮은 순서대로
-print(sorted(total.items(), key = lambda x : (-x[1],x[0]))[0][0])
+print(sorted(total, key = lambda x : (-x[1],x[0]))[0][0])
