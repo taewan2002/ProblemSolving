1877364624b8c01f2303faab3db9c4fd0b16ad65
taewan <taewan2002@naver.com>
2023-01-05 11:41:54 +0900

    [pass] brute_force

--- test/test.py
+++ test/test.py
@@ -2,7 +2,22 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-cnt = 0
-for i in range(3, N+1):
-    cnt += str(i).count("3") + str(i).count("6") + str(i).count("9")
-print(cnt)
+mlist = []
+for i in range(N):
+    tmp = list(map(int, input().split()))
+    max = 0
+    for k in range(3):
+        for j in range(k+1, 4):
+            for p in range(j+1, 5):
+                tmax = tmp[k]+tmp[j]+tmp[p]
+                if str(max)[-1] < str(tmax)[-1]:
+                    max = tmax
+    mlist.append(int(str(max)[-1]))
+
+max = 0
+save = 0
+for i in range(len(mlist)):
+    if mlist[i] >= max:
+        max = mlist[i]
+        save = i
+print(save+1)
