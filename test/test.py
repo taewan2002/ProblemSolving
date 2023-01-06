8c6b3428a1b0f8ce23dda4ecea13cabb84eaff72
taewan <taewan2002@naver.com>
2023-01-06 14:05:37 +0900

    [pass] greedy

--- test/test.py
+++ test/test.py
@@ -1,18 +1,11 @@
 import sys
-import math
 input = sys.stdin.readline
 
-fnum = [math.factorial(i) for i in range(21)]
-# 21! 부터는 범위를 벗어난다.
-N = int(input())
-
-if N==0:
-    print("NO")
-else:
-    for i in range(20, -1, -1):
-        if N >= fnum[i]:
-            N -= fnum[i]
-    if N == 0: # N을 fnum원소로 뺏을 때 0이되면 더해서 만들 수 있음
-        print("YES")
-    else:
-        print("NO")
+Alist = list(map(int, input().split()))
+Blist = list(map(int, input().split()))
+Alist.sort(reverse=True)
+Blist.sort(reverse=True)
+total = 0
+for i in range(len(Alist)):
+    total += Alist[i] * Blist[i]
+print(total)
