41136bcb76fb5a2c3e455a6474d43e79858fdadc
taewan <taewan2002@naver.com>
2023-01-07 19:15:53 +0900

    [fail] dp

---
+++ Baekjoon/dp/12015.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ arr = list(map(int, input().split()))
 dp = [0 for _ in range(N)]
 for i in range(N):
     for j in range(i):
-        if arr[i] < arr[j] and dp[i] < dp[j]:
+        if arr[i] > arr[j] and dp[i] < dp[j]:
             dp[i] = dp[j]
     dp[i] += 1
 m = max(dp)
