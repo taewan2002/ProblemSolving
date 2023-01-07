27fc34b2d5ab22f8583c9ef237dbe4299bb324d2
taewan <taewan2002@naver.com>
2023-01-07 18:24:51 +0900

    [pass] greedy

--- test/test.py
+++ test/test.py
@@ -1,23 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-for _ in range(N):
-    p = []
-
-    K = int(input())
-    for _ in range(K):
-        p.append(list(map(int, input().split())))
-    
-    p.sort()
-    temp = p[0][1]
-    cnt = 1
-
-    for i in range(K):
-        # 처음 원소로 이미 정렬을 함
-        # = 처음 원소 순위는 밀린다
-        # 두번째 원소 순위는 밀리면 안된다
-        if temp > p[i][1]:
-            cnt +=1
-            temp = p[i][1]
-    print(cnt)
+n = 1
+while True:
+    L, P, V = map(int, input().split())
+    if L==0:
+        break
+    total = 0
+    total += V // P * L
+    total += min(L, V % P)
+    print(f"Case {n}: {total}")
+    n+=1
