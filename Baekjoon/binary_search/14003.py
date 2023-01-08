39ae7dbd132bc9c32f1ee7f10cf42da2ab078116
taewan <taewan2002@naver.com>
2023-01-08 14:27:30 +0900

    [pass] binary_search

--- Baekjoon/binary_search/14003.py
+++ Baekjoon/binary_search/14003.py
@@ -1,5 +1,6 @@
 import sys
 from bisect import bisect_left
+# bisect_left(이진 탐색 코드, 같은 수일 경우 왼쪽 index를 돌려준다)
 input = sys.stdin.readline
 
 N = int(input())
@@ -9,18 +10,18 @@ lis = []
 l = 0
 for i in range(N):
     n = arr[i]
-    idx = bisect_left(lis, n)
+    idx = bisect_left(lis, n) # i가 가장 큰 숫자라면
     
-    if idx == l:
+    if idx == l: # 가장 큰숫자가 맞으면?
         lis.append(n)
         l += 1
     else:
-        lis[idx] = n
+        lis[idx] = n # 아니면 업데이트
     dp[i] = idx # dp에 저장
-print(l)
+print(l) # 길이 출력
 toPrint = [] # 역추적
 for i in range(N-1, -1, -1):
-    if dp[i] == l-1:
+    if dp[i] == l-1: # dp테이블에서 역추적으로 뽑아옴
         toPrint.append(arr[i])
         l -= 1
 print(*reversed(toPrint))
