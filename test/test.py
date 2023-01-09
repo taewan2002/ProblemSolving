1ab6c1510f0dcba401b1b1d44d6c5e6036128532
taewan <taewan2002@naver.com>
2023-01-09 22:41:33 +0900

    [fail] math

--- test/test.py
+++ test/test.py
@@ -2,10 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = list(map(int, input().split()))
-parr = [0 for _ in range(N)]
-sarr = sorted(arr)
-for i in range(len(sarr)):
-    parr[arr.index(sarr[i])] = i
-    arr[arr.index(sarr[i])] = -1
-print(*parr)
+total = 0
+divnum = 10**9+7
+for _ in range(N):
+    C, X = map(int, input().split())
+    if X-1 != 0:
+        total += C*X*(2**(X-1)) % divnum
+    else:
+        total += C*X % divnum
+    
+print(total%divnum)
