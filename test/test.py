f82866bef058880476ebe8ed878192410e13e0a6
taewan <taewan2002@naver.com>
2023-01-09 21:54:54 +0900

    [pass] sorting

--- test/test.py
+++ test/test.py
@@ -2,18 +2,10 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-K = int(input())
-start = 1 # 이분탐색 시작
-end = K # 최대값(원래 N*N인데 더 효율적인 방법)
-while start < end:
-    mid = (start + end)//2
-    
-    tmp = 0
-    for i in range(1, N+1):
-        tmp += min(mid//i, N) # mid이하의 i의 배수 or N
-        # 정렬했을 때 자기보다 작은 숫자의 갯수가 K개가 되면 답을 알 수 있다.
-    if tmp < K:
-        start = mid + 1
-    else:
-        end = mid
-print(start)
+arr = list(map(int, input().split()))
+parr = [0 for _ in range(N)]
+sarr = sorted(arr)
+for i in range(len(sarr)):
+    parr[arr.index(sarr[i])] = i
+    arr[arr.index(sarr[i])] = -1
+print(*parr)
