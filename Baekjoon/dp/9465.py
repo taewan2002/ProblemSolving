623d21b099b0ab4f07aa3360a4dcc8f41cd63a0f
taewan <taewan2002@naver.com>
2023-01-10 10:13:45 +0900

    [pass] dp

---
+++ Baekjoon/dp/9465.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+for i in range(N):
+    K = int(input())
+    dp = []
+    for _ in range(2):
+        dp.append(list(map(int, input().split())))
+    for k in range(1, K):
+        if k == 1:
+            dp[0][k] += dp[1][k-1]
+            dp[1][k] += dp[0][k-1]
+        else:
+            dp[0][k] += max(dp[1][k-1], dp[1][k-2])
+            dp[1][k] += max(dp[0][k-1], dp[0][k-2])
+    print(max(dp[0][-1], dp[1][-1]))
