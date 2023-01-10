617a9d16e5798b090948a229317d37245c950999
taewan <taewan2002@naver.com>
2023-01-10 10:48:16 +0900

    [pass] dp

--- test/test.py
+++ test/test.py
@@ -1,19 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-INF = 10000000
-N = int(input())
-arr = [list(map(int, input().split()))for _ in range(N)]
-
-total = INF
-for k in range(3):
-    dp = [[INF, INF, INF] for _ in range(N)]
-    dp[0][k] = arr[0][k]
-    for i in range(1, N):
-        for j in range(3):
-            dp[i][j] = arr[i][j] + min(dp[i-1][(j+1)%3], dp[i-1][(j+2)%3])
-    for i in range(3):
-        if i != k: # 처음, 마지막이 같지 않아야 됨
-            total = min(total, dp[-1][i])
-print(total)
-
+n = int(input())
+dp = [1,3] + [0]*(n-1)
+for i in range(2,n+1):
+    # [1, 3, 7, 17, 41]
+    # n = 2, 사자를 놓지 않는 경우 = (n=1)
+    # 사자를 놓는 경우 4 -> 7
+    # dp[i] = (dp[i-2]*3 + (dp[i-1]-dp[i-2])*2)
+    dp[i] = (dp[i-2] + dp[i-1]*2)%9901
+print(dp[-1])
