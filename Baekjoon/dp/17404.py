ce1755ca2ff7171c5c62989e1f283fd149ce155d
taewan <taewan2002@naver.com>
2023-01-10 10:28:23 +0900

    [pass] dp

---
+++ Baekjoon/dp/17404.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+INF = 10000000
+N = int(input())
+arr = [list(map(int, input().split()))for _ in range(N)]
+
+total = INF
+for k in range(3):
+    dp = [[INF, INF, INF] for _ in range(N)]
+    dp[0][k] = arr[0][k]
+    for i in range(1, N):
+        for j in range(3):
+            dp[i][j] = arr[i][j] + min(dp[i-1][(j+1)%3], dp[i-1][(j+2)%3])
+    for i in range(3):
+        if i != k: # 처음, 마지막이 같지 않아야 됨
+            total = min(total, dp[-1][i])
+print(total)
