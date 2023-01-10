0017da5821cff412eb8a88c194703aa7efb5b232
taewan <taewan2002@naver.com>
2023-01-10 15:40:56 +0900

    [fix] 1708

--- Baekjoon/convex_hull/1708.py
+++ Baekjoon/convex_hull/1708.py
@@ -3,17 +3,17 @@ def ccw(p1, p2, p3):
 
 def convex_hull(points):
     points = sorted(points)
-    lower = []
+    lower = [] # 왼쪽에서 오른쪽으로
     for p in points:
-        while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) <= 0:
+        while len(lower) >= 2 and ccw(lower[-2], lower[-1], p) <= 0: # <= 0 이면 같은 직선에 있는 점도 포함
             lower.pop()
         lower.append(p)
-    upper = []
+    upper = [] # 오른쪽에서 왼쪽으로
     for p in reversed(points):
         while len(upper) >= 2 and ccw(upper[-2], upper[-1], p) <= 0:
             upper.pop()
         upper.append(p)
-    return lower[:-1] + upper[:-1]
+    return lower[:-1] + upper[:-1] # 시작점과 끝점이 중복되므로 제거
 
 n = int(input())
 points = [list(map(int, input().split())) for _ in range(n)]
