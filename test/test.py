e46af0362eaeea71d1f83d270e7c722c305bce71
taewan <taewan2002@naver.com>
2023-01-12 08:48:33 +0900

    [pass] two pointer

--- test/test.py
+++ test/test.py
@@ -5,16 +5,21 @@ N = int(input())
 p = list(map(int, input().split()))
 check = []
 
-start = 0
-end = N-1
-minValue = abs(p[start] + p[end])
-check.append([p[start], p[end]])
-while start<end:
-    if minValue > abs(p[start] + p[end]):
-        minValue = abs(p[start] + p[end])
-        check.append([p[start], p[end]])
-    if p[start] + p[end] < 0:
-        start += 1
-    else:
-        end -= 1
+p.sort()
+minValue = int(10e9)
+for i in range(N-2):
+    fix = p[i]
+    start = i+1
+    end = N-1
+    while start<end:
+        tmp = fix + p[start] + p[end]
+        if minValue > abs(tmp):
+            minValue = abs(tmp)
+            check.append([fix, p[start], p[end]])
+        if tmp < 0:
+            start += 1
+        elif tmp > 0:
+            end -= 1
 print(*check[-1])
+
+# -97 -6 -2 6 98
