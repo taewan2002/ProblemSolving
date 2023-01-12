a968bb99366787ad99c04364193f189c624fc712
taewan <taewan2002@naver.com>
2023-01-12 21:14:08 +0900

    [pass] bfs

--- test/test.py
+++ test/test.py
@@ -1,38 +1,48 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-tmp = 1
-total = 0
-stack = []
+# 상하좌우로 탐색하기 위함
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-S = input().strip()
-for i in range(len(S)):
-    if S[i] == "(":
-        tmp *= 2
-        stack.append(S[i])
-    elif S[i] == "[":
-        tmp *= 3
-        stack.append(S[i])
-    elif S[i] == ")":
-        if not stack or stack[-1] == "[":
-            total = 0
-            break
-        if S[i-1] == "(":
-            total += tmp
-        tmp //= 2
-        stack.pop()
-    else:
-        if not stack or stack[-1] == "(":
-            total = 0
-            break
-        if S[i-1] == "[":
-            total += tmp
-        tmp //= 3
-        stack.pop()
+N = int(input())
 
-if stack:
-    total = 0
-print(total)
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().rstrip()))) # 0000 -> [0, 0, 0, 0]
 
 
-        
+def bfs(graph, a, b): # 넓이 우선 탐색
+    q = deque()
+    q.append([a, b]) # [a, b] 는 좌표값
+    cnt = 1 # 몇개의 1이 이어져 있는지 확인하기 위함
+
+    while q:
+        x, y = q.popleft() # q에 들어있는 값
+        graph[x][y] = 0 # 방문 처리
+        for i in range(4): # 상 하 좌 우로 1이 있는지 확인
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 새로운 좌표가 범위를 벗어나는 경우 continue
+            if nx < 0 or nx >= len(graph) or ny < 0 or ny >= len(graph):
+                continue
+            
+            if graph[nx][ny] == 1: # 아직 방문 안한 1이라면
+                graph[nx][ny] = 0 # 방문 처리
+                q.append([nx, ny])
+                cnt += 1
+    return cnt
+
+total = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1: # 1을 찾으면 그때부터 탐색 시작
+            count = bfs(graph, i, j)
+            total.append(count)
+
+total.sort()
+print(len(total))
+for i in total:
+    print(i)
