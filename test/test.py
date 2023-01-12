fd943cee9cbc31d2646f170816086ec4e2aaf631
taewan <taewan2002@naver.com>
2023-01-12 10:15:40 +0900

    [pass] string

--- test/test.py
+++ test/test.py
@@ -1,32 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-for i in range(N):
+for _ in range(3):
+    tmpMax = 0
+    check = [0 for _ in range(10)]
     s = input().strip()
-    start = 0
-    end = len(s)-1
-    while start<end:
-        if s[start] == s[end]:
-            start += 1
-            end -= 1
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            check[int(s[i])] += 1
         else:
-            # 오른쪽 문자만 제거하고 회문이 되는지 확인
-            if start < end - 1:
-                tmp = s[:end] + s[end+1:]
-                if tmp[:] == tmp[::-1]:
-                    print(1)
-                    break
-            # 왼쪽 문자만 제거하고 회문이 되는지 확인
-            if start + 1 < end:
-                tmp = s[:start] + s[start+1:]
-                if tmp[:] == tmp[::-1]:
-                    print(1)
-                    break
-            # 회문이 아니면 2
-            print(2)
-            break
-    if start >= end: # 회문이면 0
-        print(0)
+            tmpMax = max(tmpMax, max(check)+1)
+            check = [0 for _ in range(10)]
+    tmpMax = max(tmpMax, max(check)+1)
+    print(tmpMax)
+        
 
         
