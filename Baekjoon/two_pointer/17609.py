965414ffe072f40eae08deb007ec56d02a54ca50
taewan <taewan2002@naver.com>
2023-01-12 09:58:12 +0900

    [pass] two pointer

--- Baekjoon/two_pointer/17609.py
+++ Baekjoon/two_pointer/17609.py
@@ -11,17 +11,20 @@ for i in range(N):
             start += 1
             end -= 1
         else:
+            # 오른쪽 문자만 제거하고 회문이 되는지 확인
             if start < end - 1:
                 tmp = s[:end] + s[end+1:]
                 if tmp[:] == tmp[::-1]:
                     print(1)
                     break
+            # 왼쪽 문자만 제거하고 회문이 되는지 확인
             if start + 1 < end:
                 tmp = s[:start] + s[start+1:]
                 if tmp[:] == tmp[::-1]:
                     print(1)
                     break
+            # 회문이 아니면 2
             print(2)
             break
-    if start >= end:
+    if start >= end: # 회문이면 0
         print(0)
