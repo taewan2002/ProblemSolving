5465d384c1781f6d0542502afba2e14c6bf4a30f
taewan <taewan2002@naver.com>
2023-01-15 00:23:04 +0900

    [pass] data structure

--- test/test.py
+++ test/test.py
@@ -4,15 +4,23 @@ import heapq
 import pprint
 input = sys.stdin.readline
 
-N = int(input())
-total = []
+N, K = map(int, input().split())
+jew = []
+bag = []
 for _ in range(N):
     tmp = list(map(int, input().split()))
-    for i in tmp:
-        if len(total) < N:
-            heapq.heappush(total, i)
-        else:
-            if total[0] < i:
-                heapq.heappop(total)
-                heapq.heappush(total, i)
-print(total[0])
+    heapq.heappush(jew, [tmp[0], -tmp[1]]) # 최대 힙 구현을 위한 음수화
+for _ in range(K):
+    bag.append(int(input()))
+bag.sort() # 작은 가방부터 채운다
+
+tmp = []
+total = 0
+for i in bag:
+    while jew and jew[0][0] <= i: # 보석이 남아있고 가방보다 가볍거나 같은 경우
+        heapq.heappush(tmp, heapq.heappop(jew)[1]) # 가격만 푸시
+    if tmp:
+        total += -(heapq.heappop(tmp)) # 최댓값을 양수로 바꿔서 더해줌
+    elif not jew: # 보석이 남지 않은경우
+        break
+print(total)
