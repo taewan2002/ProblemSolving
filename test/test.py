b5d2c9a3c8aadcdbbe7611f87f6f89e21ec769a2
taewan <taewan2002@naver.com>
2023-01-14 22:47:43 +0900

    [pass] data structure

--- test/test.py
+++ test/test.py
@@ -1,54 +1,26 @@
 import sys
 from collections import deque
+import heapq
 import pprint
 input = sys.stdin.readline
 
-N, M  = map(int, input().split())
-G = []
+K, N  = map(int, input().split())
+p = list(map(int, input().split()))
+total = []
+
+for i in p:
+    heapq.heappush(total, i)
+
+minNum = 0
 for _ in range(N):
-    G.append(list(map(int, input().split())))
+    minNum = heapq.heappop(total) # 큐에서 작은 순서대로 뽑아짐
+    for i in p:
+        tmp = minNum * i # 지금 큐에 들어있는 가장 작은 수랑 곱한것을 다시 추가
+        heapq.heappush(total, tmp)
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+        if minNum % i == 0: # 5 * 2, 2 * 5 와 같은 중복 방지 한번 겹치는 순간 그 반복문에서 발생하는 수는 중복
+            break
+print(minNum) # N번 째 최솟값
 
-
-def bfs(i, j):
-    q = deque()
-    q.append([i,j])
-
-    while q:
-        x, y = q.popleft()
-        v[x][y] = True
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M:
-                if G[nx][ny] != 0 and not v[nx][ny]:
-                    q.append([nx, ny])
-                    v[nx][ny] = True
-                elif G[nx][ny] == 0:
-                    count[x][y] += 1
-
-year = 0
-while True:
-    iceberg = 0 # 빙산의 수
-    v = [[False for _ in range(M)]for _ in range(N)]
-    count = [[0 for _ in range(M)]for _ in range(N)]
-    for i in range(N):
-        for j in range(M):
-            if G[i][j] != 0 and not v[i][j]:
-                bfs(i, j)
-                iceberg += 1
-    for i in range(N):
-        for j in range(M):
-            G[i][j] -= count[i][j]
-            if G[i][j] < 0:
-                G[i][j] = 0
-    if iceberg >= 2:
-        print(year)
-        break
-    elif iceberg == 0: # 빙산이 다 녹았을 때
-        print(0)
-        break
-    year += 1
-
+# 2 5 7
+# 2 4 5
