82d7cec6537f9e1bcd708c1c7d09589ee0c4e4c2
taewan <taewan2002@naver.com>
2023-01-16 02:53:57 +0900

    [pass] data structure

--- test/test.py
+++ test/test.py
@@ -4,30 +4,32 @@ import heapq
 import pprint
 import itertools
 import math
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-
-node = [i for i in range(N+1)]
-
 def get_parent(x):
-    if node[x] == x: # 자기 스스로가 될때 까지 -> 연결의 끝까지 검색
+    if parent[x] == x: # 자기 스스로가 될때 까지 -> 연결의 끝까지 검색
         return x
-    node[x] = get_parent(node[x]) # 재귀로 연결된 끝까지 감
-    return node[x]
+    parent[x] = get_parent(parent[x]) # 재귀로 연결된 끝까지 감
+    return parent[x]
 
 def union_parant(a, b):
     a = get_parent(a)
     b = get_parent(b)
 
-    if a < b: # 작은 수를 부모로
-        node[b] = a
-    else:
-        node[a] = b
+    if a != b:
+        parent[b] = a # 앞이 부모 노드가 됨
+        number[a] += number[b] # 연결 노드수 증가
 
-for _ in range(M):
-    c, a, b = map(int, input().split())
-    if c == 0:
-        union_parant(a, b)
-    else:
-        print("YES" if get_parent(a)==get_parent(b) else "NO")
+for _ in range(int(input())):
+    parent = {}
+    number = {}
+    for _ in range(int(input())):
+        tmp = list(input().split())
+        for i in tmp:
+            if i not in parent: # 딕셔너리에 추가
+                parent[i] = i
+                number[i] = 1 # 연결 노드수
+        union_parant(tmp[0], tmp[1])
+        print(number[get_parent(tmp[0])]) # 부모노드의 연결 수     
+        
