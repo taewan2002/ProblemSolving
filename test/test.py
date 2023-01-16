bbcd183df1607122515daf32724caaa5a25d29ad
taewan <taewan2002@naver.com>
2023-01-17 02:15:58 +0900

    행렬 제곱, 분할정복

--- test/test.py
+++ test/test.py
@@ -6,19 +6,37 @@ import math
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+
+def mul(n, matrix1, matrix2):
+    t = [[0 for _ in range(N)]for _ in range(N)]
+
+    for n in range(N):
+        for k in range(N):
+            for m in range(N):
+                t[n][k] += matrix1[n][m]*matrix2[m][k]
+            t[n][k] %= 1000
+    return t
+
+def devide(n, b, matrix):
+    if b == 1:
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                matrix[i][j] %= 1000
+        return matrix
+    else:
+        tmp = devide(n, b//2, matrix) # 제곱수를 반 나눈 걸로 다시 실행
+        # 10 -> 5 -> 2, 2의 배수로 제곱 리턴하고(n^2) -> n^5 -> n^10 완성
+        if b%2 == 0: # 2의 배수면
+            return mul(n, tmp, tmp) # 제곱을 리턴함
+        else:
+            return mul(n, mul(n, tmp, tmp), matrix) # 제곱한거에 제곱을 리턴함
+
+
+N, B = map(int, input().split())
 A = []
-B = []
 for _ in range(N):
     A.append(list(map(int, input().split())))
-M, K = map(int, input().split())
-for _ in range(M):
-    B.append(list(map(int, input().split())))
 
-t = [[0 for _ in range(K)]for _ in range(N)]
-for n in range(N):
-    for k in range(K):
-        for m in range(M):
-            t[n][k] += A[n][m]*B[m][k]
-for i in t:
+total = devide(N, B, A)
+for i in total:
     print(*i)
