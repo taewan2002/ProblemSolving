d9c785a63ffaf70ab9fa3bc7ae71a225c7f51028
taewan <taewan2002@naver.com>
2023-01-17 23:31:24 +0900

    단순 그리디

--- test/test.py
+++ test/test.py
@@ -6,20 +6,18 @@ import math
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-n = int(input())
-dp = [[0, []] for _ in range(n + 1)]
-dp[1][0] = 0
-dp[1][1] = [1]
+N = int(input())
 
-for i in range(2, n+1):
-    dp[i][0] = dp[i-1][0] + 1 # 1더하기
-    dp[i][1] = dp[i-1][1] + [i] # 배열에 숫자 추가하기
-    if i % 3 == 0 and dp[i//3][0] + 1 < dp[i-1][0]: # 길이를 더 짧게 만들 수 있다면
-        dp[i][0] = dp[i//3][0] + 1
-        dp[i][1] = dp[i//3][1] + [i]
-    if i % 2 == 0 and dp[i // 2][0] + 1 < dp[i][0]:
-        dp[i][0] = dp[i // 2][0] + 1
-        dp[i][1] = dp[i // 2][1] + [i]
+v = []
+people = 0
+for _ in range(N):
+    v.append(list(map(int, input().split())))
+    people += v[-1][1]
 
-print(dp[n][0])
-print(*reversed(dp[n][1]))
+v.sort(key=lambda x: x[0])
+p = 0
+for i in range(N):
+    p += v[i][1]
+    if p >= people//2:
+        print(v[i][0])
+        break
