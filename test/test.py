7fbd3d5594273cd896117869bad19ebe0904fc2d
taewan <taewan2002@naver.com>
2023-01-18 12:45:48 +0900

    union find class 5

--- test/test.py
+++ test/test.py
@@ -5,29 +5,29 @@ import itertools
 import math
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-# 위상 정렬
+# https://www.acmicpc.net/problem/20040
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+parent = [i for i in range(n)]
+endgame = 0
 
-g = [[] for _ in range(N+1)]
-d = [0 for _ in range(N+1)]
-q = deque()
-total = []
+def get_parent(x):
+    if parent[x] == x: # 자기 스스로가 될때 까지 -> 연결의 끝까지 검색
+        return x
+    parent[x] = get_parent(parent[x]) # 재귀로 연결된 끝까지 감
+    return parent[x]
 
-for _ in range(M):
+def union(x, y, indx):
+    global endgame
+    x = get_parent(x)
+    y = get_parent(y)
+    if x != y:
+        parent[max(x,y)] = min(x,y)
+    elif endgame == 0:
+        endgame = indx
+
+for i in range(1, m+1):
     a, b = map(int, input().split())
-    g[a].append(b)
-    d[b] += 1
+    union(a, b, i)
 
-for i in range(1, N+1):
-    if d[i] == 0: # 선행 조건이 없는 수, 2가 3보다 앞에 있어야 한다 일때 2
-        q.append(i)
-
-while q:
-    tmp = q.popleft()
-    total.append(tmp)
-    for i in g[tmp]:
-        d[i] -= 1
-        if d[i] == 0:
-            q.append(i)
-print(*total)
+print(endgame)
