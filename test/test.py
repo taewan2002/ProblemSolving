1690bf5333e4a8444e439d24e9cebc7c42ad18ca
taewan <taewan2002@naver.com>
2023-01-26 09:23:36 +0900

    미로찾기, bfs

--- test/test.py
+++ test/test.py
@@ -17,22 +17,22 @@ def dijkstra():
     V[0][0] = 1
     while q:
         a, x, y = heapq.heappop(q)
-        if x == N-1 and y == N-1:
+        if x == N-1 and y == M-1:
             print(a)
             return
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<N and V[nx][ny] == 0:
+            if 0<=nx<N and 0<=ny<M and V[nx][ny] == 0:
                 V[nx][ny] = 1
-                if G[nx][ny] == 0: # 검은방일때 방을 부시는 횟수 추가
+                if G[nx][ny] == 1:
                     heapq.heappush(q, [a+1, nx, ny])
                 else:
                     heapq.heappush(q, [a, nx, ny])
 
-N = int(input())
+M, N = map(int, input().split())
 G = []
 for _ in range(N):
     G.append(list(map(int, input()[:-1])))
-V = [[0 for _ in range(N)] for _ in range(N)]
+V = [[0 for _ in range(M)] for _ in range(N)]
 dijkstra()
