bca6120a588621e93421ea60f097443e39595149
taewan <taewan2002@naver.com>
2023-01-26 09:23:30 +0900

    미로찾기, bfs

---
+++ Baekjoon/bfs/1261.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+import heapq
+# import itertools
+# import math
+# import bisect
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+INF = sys.maxsize
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0 ,0]
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, [0, 0, 0])
+    V[0][0] = 1
+    while q:
+        a, x, y = heapq.heappop(q)
+        if x == N-1 and y == M-1:
+            print(a)
+            return
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<N and 0<=ny<M and V[nx][ny] == 0:
+                V[nx][ny] = 1
+                if G[nx][ny] == 1:
+                    heapq.heappush(q, [a+1, nx, ny])
+                else:
+                    heapq.heappush(q, [a, nx, ny])
+
+M, N = map(int, input().split())
+G = []
+for _ in range(N):
+    G.append(list(map(int, input()[:-1])))
+V = [[0 for _ in range(M)] for _ in range(N)]
+dijkstra()
