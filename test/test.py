a7d56c1e4f9c75e9d31de7144c5f3d6a422fae80
taewan <taewan2002@naver.com>
2023-01-29 17:35:57 +0900

    투포인터 수고르기

--- test/test.py
+++ test/test.py
@@ -8,20 +8,17 @@ sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 INF = sys.maxsize
 
-def f(x, y, w):
-    h1 = math.sqrt(math.pow(x, 2)-math.pow(w, 2))
-    h2 = math.sqrt(math.pow(y, 2)-math.pow(w, 2))
-    c = h1*h2/(h1+h2)
-    return c
-
-x, y, c = map(float, input().split())
-s, e = 0, min(x,y)
-total = 0
-while e-s > 0.000001:
-    mid = (s+e)/2
-    if f(x, y, mid) >= c:
-        total = mid
-        s = mid
+N, M = map(int, input().split())
+arr = []
+for _ in range(N):
+    arr.append(int(input()))
+arr.sort()
+s, e = 0, 1
+minNum = INF
+while s<len(arr) and e<len(arr):
+    if arr[e] - arr[s] >= M:
+        minNum = min(minNum, arr[e] - arr[s])
+        s += 1
     else:
-        e = mid
-print(f"{total:.3f}")
+        e += 1
+print(minNum)
